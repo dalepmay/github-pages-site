414ee5b8e8c4ef98a321224fbb1a418d2986ca7e
Dale P May <78275759+dalepmay7@users.noreply.github.com>
2024-10-15 12:43:44 -0500

    update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="text-center">
+      <h2>Welcome</h2>
+      <p>This is a work in progress...</p>
+    </div>
+  );
+}
+
+export default Home;
